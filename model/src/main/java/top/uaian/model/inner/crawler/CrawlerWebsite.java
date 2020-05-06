c942102fece563484106b78c596b7fafe5533dc8
xukainan <xukainan@fang.com>
2020-05-06 17:56:54 +0800

    忽略maven模块

---
+++ model/src/main/java/top/uaian/model/inner/crawler/CrawlerWebsite.java
@@ -0,0 +1,13 @@
+package top.uaian.model.inner.crawler;
+
+import lombok.Data;
+
+/**
+ * description:  爬虫网站<br>
+ * date: 2020/5/6 17:45 <br>
+ * @author: xukainan <br>
+ * version: 1.0 <br>
+ */
+@Data
+public class CrawlerWebsite {
+}
